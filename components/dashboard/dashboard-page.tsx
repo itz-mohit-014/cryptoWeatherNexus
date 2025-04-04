7609c200a0adec17052591bedf0005a51ce8117c
Mohit Jangid <95575123+itz-mohit-014@users.noreply.github.com>
2025-04-04 08:17:54 +0530

    Update dashboard-page.tsx enable news fetching...

--- components/dashboard/dashboard-page.tsx
+++ components/dashboard/dashboard-page.tsx
@@ -19,7 +19,7 @@ export function DashboardPage() {
 
     dispatch(fetchWeatherData())
     dispatch(fetchInitialCryptoData());
-  // dispatch(fetchNewsData())
+    dispatch(fetchNewsData())
 
   }, [dispatch])
 
